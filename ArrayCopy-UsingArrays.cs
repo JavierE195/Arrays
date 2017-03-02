1634973f59fa7a447d574f37938bf1f2199a2de4
JavierE195 <ellerbrockjavier@hotmail.com>
2017-03-02 11:25:47 -0600

    Add files via upload
    Copies the elements of an array into other array.

---
+++ ArrayCopy-UsingArrays.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert the number of elements in the list: ");// array's length
+            int elementsNumber = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[elementsNumber];
+            int length = numbers.Length - 1;
+
+            for (int i = 0; i <= length; i++)
+            {
+                Console.Write("Insert element number " + (i + 1) + " : ");
+                int individualNumber = int.Parse(Console.ReadLine());
+                numbers[i] = individualNumber;
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Here is the list: ");
+            for (int o = 0; o < numbers.Length; o++)
+            {
+                Console.WriteLine(numbers[o]);
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Here is a copy of the list: ");
+            int[] result = Copy(numbers);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static int[] Copy(int[] n)
+        {
+            int[] arrayCopy = new int[n.Length];
+            for (int u = 0; u < n.Length; u++)
+            {
+                arrayCopy[u] = n[u];
+            }
+            return arrayCopy;
+        }
+
+    }
+}
